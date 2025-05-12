96fd33c589c21e777cfeaddc97306dbe48065bba
ErrorC137 <154239085+ErrorC137@users.noreply.github.com>
2025-05-12 22:31:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,23 @@
-const XRPL_TESTNET_WS = "wss://s.altnet.rippletest.net:51233";
-const F2J_TOKEN = {
-    currency: "F2J",
-    issuer: "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe",
-    value: "0"
+const CONFIG = {
+  ENV: 'testnet',
+  NETWORKS: {
+    testnet: {
+      WS_ENDPOINT: 'wss://s.altnet.rippletest.net:51233',
+      EXPLORER: 'https://testnet.xrpl.org'
+    },
+    mainnet: {
+      WS_ENDPOINT: 'wss://xrplcluster.com',
+      EXPLORER: 'https://livenet.xrpl.org'
+    }
+  },
+  TOKEN: {
+    SYMBOL: 'F2J',
+    ISSUER: 'rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe',
+    DECIMALS: 6,
+    TRUST_LIMIT: '1000000'
+  },
+  PLATFORM: {
+    WALLET: 'rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe',
+    EMAIL: 'support@freetojoy.org'
+  }
 };
-const PLATFORM_WALLET = "rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe";
-
